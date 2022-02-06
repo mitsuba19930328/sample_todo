136edfd4c217a9013ec28a08c1fe79b292e796f3
rui-shimazaki <rui-shimazaki@cuebic.co.jp>
2022-02-07 00:08:47 +0900

    [ADD]カードの追加処理実装

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
+  // Todoリストのデータ
   final String title;
 
   @override
@@ -32,6 +32,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> todoList = [];
   int _counter = 0;
 
   // todoチケット作成メソッド
@@ -62,45 +63,64 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
 
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-
-        children: [
-          for (int i=0; i<3; i++)
-          Container(
-            child: Card(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const ListTile(
-                    leading: Icon(Icons.album),
-                    title: Text('The Enchanted Nightingale'),
-                    subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      TextButton(
-                        child: const Text('完了'),
-                        onPressed: () {/* ... */},
-                      ),
-                      const SizedBox(width: 8),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () { // ボタン押したときに実行される
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => CreateTodoPage(), // SecondPageは遷移先のクラス
+      // データを元にListViewを作成
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(todoList[index]),
             ),
           );
+        },
+      ),
+
+      // body: Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //
+      //   children: [
+      //     for (int i=0; i<3; i++)
+      //     Container(
+      //       child: Card(
+      //         child: Column(
+      //           mainAxisSize: MainAxisSize.min,
+      //           children: <Widget>[
+      //             const ListTile(
+      //               leading: Icon(Icons.album),
+      //               title: Text('The Enchanted Nightingale'),
+      //               subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+      //             ),
+      //             Row(
+      //               mainAxisAlignment: MainAxisAlignment.end,
+      //               children: <Widget>[
+      //                 TextButton(
+      //                   child: const Text('完了'),
+      //                   onPressed: () {/* ... */},
+      //                 ),
+      //                 const SizedBox(width: 8),
+      //               ],
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async { // ボタン押したときに実行される
+          final newTodoMemo = await Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) {
+                // 遷移先の画面としてリスト追加画面を指定
+                return CreateTodoPage();
+              }),
+          );
+          if (newTodoMemo != null) {
+            setState(() {
+              // リスト追加
+              todoList.add(newTodoMemo);
+            });
+            // キャンセルした場合は newListText が null となるので注意
+          }
         },
         tooltip: 'todoチケットの新規作成',
         child: const Icon(Icons.add),
